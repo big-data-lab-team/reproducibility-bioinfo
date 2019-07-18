84271645fe116a3f99d6e0f18d37f11a6bd9a5e8
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2019-07-18 18:37:27 -0400

    applied changes being requested in the pull request

--- extractFeature.py
+++ extractFeature.py
@@ -1,48 +1,52 @@
-#============================
-#PACKAGES
-#============================
+# cSpell:disable
+# ============================
+# PACKAGES
+# ============================
 import argparse
 import sys
 import json
 import time
 import traceback
-from helpers import extractAAC,log,readFile
+from helpers import extractAAC, log, readFile
 
 
-#============================
-#MAIN
-#============================
+# ============================
+# MAIN
+# ============================
 def main():
 
-    #Extracting feature from Dataset files
+    # Extracting feature from Dataset files
     try:
-        #Checking for right argument from user
-        parser = argparse.ArgumentParser(description='Extracting Features from Dataset')
+        # Checking for right argument from user
+        parser = argparse.ArgumentParser(
+            description='Extracting Features from Dataset')
         parser.add_argument('feature', type=str, help='Feature to Extract')
-        parser.add_argument('dataset', type=str, help='Dataset from which the feature would be Extracted from (trainTest or Independent)')
+        parser.add_argument(
+            'dataset', type=str, help='Dataset from which the feature would be Extracted from (trainTest or Independent)')
         parser.parse_args()
 
-        #Logging start of Program
+        # Logging start of Program
         feature = parser.parse_args().feature
         dataset = parser.parse_args().dataset
-        
-        log(time.strftime("%m/%d/%Y, %H:%M:%S",time.localtime()))
-        # log("Running extractFeature.py  << feature >> "+feature+" << dataset >> "+dataset)
-        
-        #Loading Json
-        config=readFile("/config.json")
-        
-        #Config Json File
-        configJson = json.loads(config)  
 
-        #Features
-        if feature=='aac':
-            extractAAC(configJson,dataset,feature)
+        log(time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime()))
+        # log("Running extractFeature.py  << feature >> "+feature+" << dataset >> "+dataset)
+
+        # Loading Json
+        config = readFile("config.json")
+
+        # Config Json File
+        configJson = json.loads(config)
+
+        # Features
+        if feature == 'aac':
+            extractAAC(configJson, dataset, feature)
             log("AAC being extracted from [ "+dataset+" ] successfully!!!")
-        
+
     except Exception:
-        log("[ "+sys.argv[1]+" ] Feature Extraction Process Failed!!")
+        log("[ "+feature+" ] Feature Extraction Process Failed!!")
         log(traceback.format_exc())
+
 
 if __name__ == "__main__":
     main()
--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 # cSpell:disable
-#============================
-#PACKAGES
-#============================
+# ============================
+# PACKAGES
+# ============================
 
 import os
 import requests
@@ -9,37 +9,45 @@ import traceback
 import time
 import numpy as np
 
-#============================
-#LOG HANDLER
-#============================
+# ============================
+# LOG HANDLER
+# ============================
+
+
 def log(message):
     try:
-        logHandler=open((os.getcwd()+"/log.log"), 'a')
+        logHandler = open(os.path.join(os.getcwd(), "log.log"), 'a')
         print(message)
         logHandler.write(message)
         logHandler.write(os.linesep)
         logHandler.close()
     except Exception:
-        print (traceback.format_exc())
+        print(traceback.format_exc())
 
-#============================
-#FILE READER
-#============================
+# ============================
+# FILE READER
+# ============================
+
+
 def readFile(fName):
     try:
-        fileHandler=open((os.getcwd()+fName), 'r')
+        if fName[0] == os.sep or fName[0] == "/":
+            fName = fName[1:]
+        fileHandler = open(os.path.join(os.getcwd(), fName), 'r')
         log("Loading "+fName+" ...")
-        rawFile=fileHandler.read()
+        rawFile = fileHandler.read()
         fileHandler.close()
         return rawFile
     except Exception:
         raise Exception(traceback.format_exc())
-#============================
-#PROTEIN FASTA DOWNLOADER
-#============================
-def getFileFromURL(webAddress,pFile,extension):
+# ============================
+# PROTEIN FASTA DOWNLOADER
+# ============================
+
+
+def getFileFromURL(webAddress, pFile, extension):
     try:
-        URL=webAddress.format(pFile,extension)
+        URL = webAddress.format(pFile, extension)
         # log("Sending request to ... ")
         # log(URL)
         response = requests.get(URL)
@@ -49,131 +57,119 @@ def getFileFromURL(webAddress,pFile,extension):
         else:
             raise Exception(
                 'Online Request Status for {} is {}'.format(
-                    pFile+"."+extension,response.status_code))
+                    pFile+"."+extension, response.status_code))
 
     except Exception:
         raise Exception(traceback.format_exc())
-#============================
-#FASTA PARSER
-#============================
+# ============================
+# FASTA PARSER
+# ============================
+
+
 def parseFasta(content):
     try:
-        sequence = ("".join(content[1:]).replace(os.linesep,"")).replace(" ","")
+        sequence = ("".join(content[1:]).replace(
+            os.linesep, "")).replace(" ", "")
         log("Processing Downloaded Data for Sequence ...")
         return sequence
     except Exception:
         raise Exception(traceback.format_exc())
-        
 
-#============================
-#DOWNLOAD/PARSE/WRITE TO FILE
-#============================
-def downloadCreateDataList(itemList,site,format,dataPath):
+
+# ============================
+# DOWNLOAD/PARSE/WRITE TO FILE
+# ============================
+def downloadCreateDataList(itemList, site, format, dataPath):
     try:
         try:
             open((os.getcwd()+dataPath), 'w').close()
-            
+
         except Exception:
-            newPath=dataPath.split("/")
+            newPath = dataPath.split("/")
             path = ("/".join(newPath[:(len(newPath)-1)]))
             os.mkdir(os.getcwd()+path)
-            
+
         finally:
-            fileHandler=open((os.getcwd()+dataPath), 'a')
+            fileHandler = open((os.getcwd()+dataPath), 'a')
 
-
-        lastItemInList=itemList[len(itemList)-1]
+        lastItemInList = itemList[len(itemList)-1]
 
         for item in itemList:
-            log( "Downloading ... "+item)
-            callResult=getFileFromURL(site,item,format)
-                
+            log("Downloading ... "+item)
+            callResult = getFileFromURL(site, item, format)
+
             parseResult = parseFasta(callResult.split(os.linesep))
-                
+
             fileHandler.write(">")
             fileHandler.write(item)
             fileHandler.write(os.linesep)
             fileHandler.write(parseResult)
-            
-            if item!=lastItemInList:
+
+            if item != lastItemInList:
                 fileHandler.write(os.linesep)
-        
+
         fileHandler.close()
 
     except Exception:
         raise Exception(traceback.format_exc())
 
-#============================
-#Amino Percentage based
-# a Sequence and a Letter
-#============================
-def calcAminoPercentage(seq,letter):
-  counter=0
-  for alphabet in seq:
-    if (alphabet==letter):
-      counter=counter+1
-  
-  return ((counter/len(seq))*100)
 
-#============================
-#Rounds Floating Points Number
-#============================
-def roundFloatingpoint(num):
-  return (np.round(num*1000)/1000)
+# ============================
+# Removes Undesired Character
+# ============================
 
-#============================
-#Removes Undesired Character
-#============================
+
 def removeSpaceNewLine(seq):
-  return ((seq.replace('\n','')).replace(' ',''))
+    return ((seq.replace(os.linesep, '')).replace(' ', ''))
 
-#============================
+# ============================
 # Creating Directory (if does not exist)
-# Clearing the file (if already exist) 
-#============================
+# Clearing the file (if already exist)
+# ============================
+
+
 def clearFile(fileAddress):
-    #Trying to create directories (If does not exist)
+        # Trying to create directories (If does not exist)
     try:
         open((os.getcwd()+fileAddress), 'w').close()
     except:
-        newPath=(fileAddress).split("/")
-        path = ("/".join(newPath[:(len(newPath)-1)]))
-        os.mkdir(os.getcwd()+path)
+        newPath = (fileAddress).split((os.sep))
+        path = ((os.sep).join(newPath[:(len(newPath)-1)]))
+        os.makedirs((os.getcwd()+path), mode=0o777, exist_ok=False)
     finally:
         open((os.getcwd()+fileAddress), 'w').close()
 
-#============================
-#Calculates AAC(each Sequence)
-#============================
-def calcSeqAAC(sequence,letters):
+# ============================
+# Calculates AAC(each Sequence)
+# ============================
+
+
+def calcSeqAAC(sequence, letters):
     sequence = removeSpaceNewLine(sequence)
-    aminoPercentage=[]
-
-    for amino in letters:
-
-        aminoPercentage.append(
-            roundFloatingpoint(
-                calcAminoPercentage(sequence,amino)))
+    aminoPercentage = [(np.round(
+        ((sequence.count(amino))/len(sequence))*100, 3)) for amino in letters]
 
     return aminoPercentage
 
-#============================
-#Calculates AAC(each File)
-#============================
-def calcAAC(aminoLetters,source,label,destination):
+# ============================
+# Calculates AAC(each File)
+# ============================
+
+
+def calcAAC(aminoLetters, source, label, destination):
     try:
         # Reading Source Transporter
-        sourceHandler=open(source,'r')
-        sourceData=(sourceHandler.read()).split("\n")
+        sourceHandler = open(source, 'r')
+        sourceData = (sourceHandler.read()).split(os.linesep)
         sourceHandler.close()
 
-        #Calculating, Writing to File
+        # Calculating, Writing to File
         for line in sourceData:
             if '>' not in line:
-                #Calculating the AAC feature and Writes it to aac.csv
-                destination.write('\n'+
-                    (str(
-                        calcSeqAAC(line,aminoLetters)).replace('[','')).replace(']','')+","+label)
+                # Calculating the AAC feature and Writes it to aac.csv
+                destination.write(os.linesep +
+                                  (str(
+                                      calcSeqAAC(line, aminoLetters)).replace('[', '')).replace(']', '')+","+label)
             # else:
             #     if '>' in line:
             #         log("calculating AAC for "+ line)
@@ -181,71 +177,81 @@ def calcAAC(aminoLetters,source,label,destination):
     except Exception:
         log(traceback.format_exc())
         raise Exception(traceback.format_exc())
-    
-        
-#============================
-#Extract AAC
-#============================
-def extractAAC(config,dataset,feature):
-    
-    try:
-        aminoLetters=['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
 
-        source = config["datasetPath"].format(dataset)    
-        destination1= config["featuresPath"].format(dataset,feature+"7.csv")
-        destination2= config["featuresPath"].format(dataset,feature+"8.csv")
+
+# ============================
+# Extract AAC
+# ============================
+def extractAAC(config, dataset, feature):
+
+    try:
+        aminoLetters = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
+                        'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+
+        source = config["datasetPath"].format(dataset)
+        destination1 = config["featuresPath"].format(dataset, feature+"7.csv")
+        destination2 = config["featuresPath"].format(dataset, feature+"8.csv")
 
         # Creating Directory (if does not exist)
-        #Clearing the file (if already exist)
+        # Clearing the file (if already exist)
         clearFile(destination1)
         clearFile(destination2)
-        
-        #Logging
+
+        # Logging
         log("Source : "+source)
         log("destination : "+destination1)
         log("destination : "+destination2)
 
-        #List DataFiles from the Source
+        # List DataFiles from the Source
         log("Listing data files from "+source)
-        trasnporters=[]
-        nonTransporters=[]
+        # trasnporters = [file[:-6] for file in os.listdir(os.getcwd()+source) if "nonTransporter" not in file]
+        # nonTransporters = [file[:-6] for file in os.listdir(os.getcwd()+source) if "nonTransporter" in file]
+
+        trasnporters = []
+        nonTransporters = []
+
         for file in os.listdir(os.getcwd()+source):
             if "nonTransporter" in file:
                 nonTransporters.append(file[:-6])
             else:
                 if "fasta" in file:
                     trasnporters.append(file[:-6])
-        
-        
-        #AAC1
+
+        # AAC1
         # Transporters (amino-cation-anion-electron-sugar-protein-other)
         # Opening Destination1 and Printing the Header
-        aac1=open((os.getcwd()+destination1), 'a')
-        aac1.write((str(aminoLetters).replace('[','')).replace(']','')+",Label")
+        aac1 = open((os.getcwd()+destination1), 'a')
+        aac1.write((str(aminoLetters).replace(
+            '[', '')).replace(']', '')+",Label")
 
-        log("Extraicting AAC from "+dataset+" for 7 classes")
+        log("Extracting AAC from "+dataset+" for 7 classes")
         for transporter in trasnporters:
-            sourceFile=os.getcwd()+config["datasetPath"].format(dataset)+transporter+".fasta"
-            calcAAC(aminoLetters,sourceFile,transporter,aac1)
-        
-        #Closing destination1
+            sourceFile = os.getcwd() + \
+                config["datasetPath"].format(dataset)+transporter+".fasta"
+            calcAAC(aminoLetters, sourceFile, transporter, aac1)
+
+        # Closing destination1
         aac1.close()
 
-        #AAC2
+        # AAC2
         # Transporter vs non-Transporters
         # Opening Destination1 and Printing the Header
-        aac2=open((os.getcwd()+destination2), 'a')
-        aac2.write((str(aminoLetters).replace('[','')).replace(']','')+",Label")
+        aac2 = open((os.getcwd()+destination2), 'a')
+        aac2.write((str(aminoLetters).replace(
+            '[', '')).replace(']', '')+",Label")
 
-        log("Extraicting AAC from "+dataset+" for 8 classes(Including non-transporters)")
+        log("Extracting AAC from "+dataset +
+            " for 8 classes(Including non-transporters)")
         for transporter in trasnporters:
-            sourceFile=os.getcwd()+config["datasetPath"].format(dataset)+transporter+".fasta"
-            calcAAC(aminoLetters,sourceFile,"transporter",aac2)
-        
-        sourceFile=os.getcwd()+config["datasetPath"].format(dataset)+"nonTransporter"+".fasta"
-        calcAAC(aminoLetters,sourceFile,"nonTransporter",aac2)
-        
-        #Closing destination2
+            sourceFile = os.getcwd() + \
+                config["datasetPath"].format(dataset)+transporter+".fasta"
+            calcAAC(aminoLetters, sourceFile, "transporter", aac2)
+
+        sourceFile = os.getcwd() + \
+            config["datasetPath"].format(dataset)+"nonTransporter"+".fasta"
+        calcAAC(aminoLetters, sourceFile, "nonTransporter", aac2)
+
+        # Closing destination2
         aac2.close()
 
     except Exception:
